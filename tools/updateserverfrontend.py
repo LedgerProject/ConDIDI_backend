8cbfde7c969bba35626bac455c1b7b475f5e2fb4
Ingo Keck <Ingo.Keck@tib.eu>
2021-03-18 15:28:31 +0100

    bugfix

--- tools/updateserverfrontend.py
+++ tools/updateserverfrontend.py
@@ -1,6 +1,5 @@
 from bottle import request, route, template, run
 import subprocess
-import tempfile
 
 USER=""
 PASS=""
@@ -11,25 +10,29 @@ def do_update():
     result = subprocess.run(["git", "pull"], timeout=30, capture_output=True, text=True, cwd=DIRNAME)
     out += result.stdout
     out += result.stderr
+    print(result.stdout)
     result = subprocess.run(["npm", "install", "--force"], timeout=120, capture_output=True, text=True, cwd=DIRNAME)
     out += result.stdout
     out += result.stderr
+    print(result.stdout)
     result = subprocess.run(["npm", "run", "build"], timeout=120, capture_output=True, text=True, cwd=DIRNAME)
     out += result.stdout
     out += result.stderr
+    print(result.stdout)
     result = subprocess.run(["cp", "-r", "build/*", "/var/ww/html/condidi/"], timeout=30, capture_output=True, text=True, cwd=DIRNAME)
     out += result.stdout
     out += result.stderr
+    print(result.stdout)
     return out
 
 @route('/updateserver')
 def index():
-    username = request.forms.get('user')
-    password = request.forms.get('pass')
-    if username != USER:
-        return "Nada"
-    if password != PASS:
-        return "Nada"
+    #username = request.forms.get('user')
+    #password = request.forms.get('pass')
+    #if username != USER:
+    #    return "Nada"
+    #if password != PASS:
+    #    return "Nada"
     result = do_update()
     return result
 
